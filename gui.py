c66b253230da7432e3f8830c08156b9e92ea9461
majikaisteve123456 <411610827@qq.com>
2018-09-05 08:38:53 +0800

    gui
    未带文件操作，剩下开发天数完成文件预览和保存操作

--- gui.py
+++ gui.py
@@ -1,37 +1,42 @@
 import tkinter as tk
 from tkinter import *
 from tkinter.messagebox import showinfo,showwarning,showerror #各种类型的提示框
+from tkinter.scrolledtext import ScrolledText
+
+global list
+list=[]
+class App:
+    entry1 = None
+    entry2 = None
+    text4 = None
+    def __init__(self,top):
+        # 放置label和键盘输入框
+        text1 = Label(top, text="发送地址: ").grid(row=0, sticky=W)
+        App.entry1 = Entry(top)  # 实例化一个输入框
+        App.entry1.grid(column=0, row=0)
+        text2 = Label(top, text="邮件发送数目: ").grid(row=1, sticky=W)
+        App.entry2 = Entry(top)  # 实例化一个输入框
+        App.entry2.grid(column=0, row=1)
+        text3 = Label(top, text="发送内容:").grid(row=2, sticky=W)
+        # 文本框text组件
+        App.text4 = ScrolledText(top, width=40, height=10)
+        App.text4.grid(sticky=W)
+        # 发送键
+        button2 = Button(top, width=15, text='发送', command=self.show_entry_fields).grid(row=5, column=0, pady=4)
+
+        # 监听键盘事件,按确定后
+    def show_entry_fields(self):
+         list.append(App.entry1.get())
+         list.append(App.entry2.get())
+         s=App.text4.get("0.0", "end")
+         list.append(s.strip())
+         print(list)
+
+
 #顶层窗口
 top=tk.Tk()
 top.geometry('400x300')#设置初始化窗口大小
 top.title("MailBomb")#设置名称
-
-#监听键盘事件
-def show_entry_fields():
-    print("First Name: %s" % entry1.get())
-
-#放置label和键盘输入框
-text1=Label(top,text="发送地址:").grid(row=0)
-entry1 = Entry(top)#实例化一个输入框
-entry1.grid(row=0,column=1)
-
-
-
-text2=Label(top,text="发送内容:").grid(row=1)
-entry2 = Entry(top)#实例化一个输入框
-entry2.grid(row=1,column=1)
-
-
-text3=Label(top,text="邮件发送数目:").grid(row=2)
-entry3 = Entry(top)#实例化一个输入框
-entry3.grid(row=2,column=1)
-
-Button(top, text='退出', command=top.quit).grid(row=3, column=0, sticky=W, pady=4)
-Button(top, text='确定', command=show_entry_fields).grid(row=3, column=1, sticky=W, pady=4)
-
-
-text4=Label(top,text="文件上传")
-
-text5=Label(top,text="文件预览")
-
+app=App(top)
 top.mainloop()
+
